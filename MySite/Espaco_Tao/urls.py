91782815df403618731c7f2c797c95ed3246bb2b
V0l_B1nary <74317064+v0lb1nary@users.noreply.github.com>
2021-02-09 15:23:30 -0300

    Create All auth.views

--- MySite/Espaco_Tao/urls.py
+++ MySite/Espaco_Tao/urls.py
@@ -5,12 +5,29 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 
+    #& Home
     path("home/", views.home_pag, name="home"),
 
+
+    #& Reazlização do Login
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("painel_controle/", views.painel_controle_pag, name="painel_controle"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 
+
+    #& Cadastramento
     path("cadastro/", views.cadastro_cliente, name="cadastro")
+
+
+    #& Alteração de Senha
+    path('alterar_senha/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('alterar_senha/alterada/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+
+    #& Redefinição de Senha
+    path('reiniciar_senha/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reiniciar_senha/reiniciada', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reiniciar/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reiniciar/reiniciada/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
